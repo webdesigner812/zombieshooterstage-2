61da26fbe5c45ff7c44ff7b454d95f63b749b143
webdesigner812 <77376988+webdesigner812@users.noreply.github.com>
2022-02-12 16:10:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,111 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting;
+var zombie, zombieImg;
+
+var heart1, heart2, heart3;
+var heart1Img, heart2Img, heart3Img;
+
+var zombieGroup;
+
+
+
+function preload(){
+  
+
+  shooterImg = loadImage("assets/shooter_2.png")
+  shooter_shooting = loadImage("assets/shooter_3.png")
+
+  zombieImg = loadImage("assets/zombie.png")
+
+  bgImg = loadImage("assets/bg.jpeg")
+
+}
+
+function setup() {
+
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //adding the background image
+  bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
+bg.addImage(bgImg)
+bg.scale = 1.1
+  
+
+//creating the player sprite
+player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
+ player.addImage(shooterImg)
+   player.scale = 0.3
+   player.debug = true
+   player.setCollider("rectangle",0,0,300,300)
+
+
+   //creating sprites to depict lives remaining
+   heart1 = createSprite(displayWidth-150,40,20,20)
+   heart1.visible = false
+    heart1.addImage("heart1",heart1Img)
+    heart1.scale = 0.4
+
+    
+    //creating group for zombies    
+   
+}
+
+function draw() {
+  background(0); 
+
+  //moving the player up and down and making the game mobile compatible using touches
+if(keyDown("UP_ARROW")||touches.length>0){
+  player.y = player.y-30
+}
+if(keyDown("DOWN_ARROW")||touches.length>0){
+ player.y = player.y+30
+}
+
+
+//release bullets and change the image of shooter to shooting position when space is pressed
+if(keyWentDown("space")){
+  
+  player.addImage(shooter_shooting)
+  
+ 
+}
+
+//player goes back to original standing image once we stop pressing the space bar
+else if(keyWentUp("space")){
+  player.addImage(shooterImg)
+}
+
+
+//destroy zombie when player touches it
+if(zombieGroup.isTouching(player)){
+ 
+
+ 
+}
+
+//calling the function to spawn zombies
+enemy();
+
+drawSprites();
+}
+
+
+
+//creating function to spawn zombies
+function enemy(){
+  if(frameCount%50===0){
+
+    //giving random x and y positions for zombie to appear
+    zombie = createSprite(random(500,1100),random(100,500),40,40)
+
+    zombie.addImage(zombieImg)
+    zombie.scale = 0.15
+    zombie.velocityX = -3
+    zombie.debug= true
+    zombie.setCollider("rectangle",0,0,400,400)
+   
+    
+  }
+
+}
